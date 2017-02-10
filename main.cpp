ced07d6fd51e7b93d23f7c72dcdd9b1492b66641
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-02-09 19:20:15 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+vector<int> replace(vector<int> min_heap, int replaced_value, int replacement)
+{
+    return min_heap;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
